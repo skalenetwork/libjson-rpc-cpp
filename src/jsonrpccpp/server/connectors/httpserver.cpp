44a7b9b5db2b85ab8ffce123c8be8a92c6a1a0b1
Oleh Nikolaiev <oleg@skalelabs.com>
2021-02-17 17:45:57 +0200

    SKALE-3679 add mutex

--- src/jsonrpccpp/server/connectors/httpserver.cpp
+++ src/jsonrpccpp/server/connectors/httpserver.cpp
@@ -71,6 +71,8 @@ static gnutls_x509_crt_t get_client_certificate(gnutls_session_t tls_session) {
     return NULL;
   }
 
+  const std::lock_guard< std::mutex > lock( certs_mutex );
+
   std::vector<uint8_t> pcert_data(pcert.size);
   for (size_t i = 0; i < pcert.size; ++i) {
     pcert_data[i] = pcert_data.data[i];
